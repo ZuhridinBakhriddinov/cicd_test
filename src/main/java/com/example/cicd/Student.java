f766c94dd78d101a79676225d166ba38edca5c10
zuhridinbakhriddinov1 <zukicgleo@gmail.com>
2024-02-05 21:51:42 +0500

    hi

---
+++ src/main/java/com/example/cicd/Student.java
@@ -0,0 +1,15 @@
+package com.example.cicd;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/example/cicd/StudentController.java
@@ -0,0 +1,18 @@
+package com.example.cicd;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudent() {
+        return List.of(new Student("Alisher", 22), new Student("Baxtiyor", 23));
+    }
+}
